208ef463b3fcbf849b48b408a546a23703424864
ApexioDaCoder <apexiodacoder@gmail.com>
2020-08-18 16:29:01 -0500

    Update pageMenu.js

--- src/common/pageMenu.js
+++ src/common/pageMenu.js
@@ -91,7 +91,7 @@ class PageMenu {
                   text     : `${
                     el.footer ? el.footer.text + ' | ' :
                     ''}${
-                    embed.footer ? (el.footer ? embed.footer.text + ' | ' :
+                    embed.footer ? (el.footer ? el.footer.text + ' | ' :
                     embed.footer.text + ' | ') :
                     ''}${index + 1}/${data.length}`,
                   icon_url : (el.footer || embed.footer || { footer: { icon_url: undefined } }).icon_url,
